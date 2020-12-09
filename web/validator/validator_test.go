1eb346f7ed179c8ef024e3bbbc1ddfd1928cd2cd
ZhengYu <zhengy@gato.cn>
2020-12-09 16:47:20 +0800

    thinking in golang

---
+++ web/validator/validator_test.go
@@ -0,0 +1,40 @@
+package validator
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"testing"
+)
+
+type User struct {
+	Remarks string `json:"remarks" validate:"lte=2"`
+}
+
+func Test_validatorApi(t *testing.T) {
+	user := &User{
+		Remarks: "中国",
+	}
+
+	vd := validator.New()
+	err := vd.Struct(user)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+			fmt.Println(err.Namespace())
+			fmt.Println(err.Field())
+			fmt.Println(err.StructNamespace())
+			fmt.Println(err.StructField())
+			fmt.Println(err.Tag())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Type())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println()
+		}
+	}
+}
